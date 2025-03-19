f44cea099e9f3737d002cdb2d91d5deb3d49982c
Syed <syed.gohar@homelifemedia.com>
2025-03-19 17:44:57 +0500

    new added

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const OWNERREZ_USERNAME = process.env.OWNERREZ_USERNAME;
 const OWNERREZ_PASSWORD = process.env.OWNERREZ_PASSWORD;
 
 // Encode credentials for Basic Auth
-const basicAuth = `Basic ${base64.encode(`${OWNERREZ_USERNAME}:${OWNERREZ_PASSWORD}`)}`;
+const basicAuth = `Basic ${base64.encode(
+  `${OWNERREZ_USERNAME}:${OWNERREZ_PASSWORD}`
+)}`;
 
 // Function to create a guest in OwnerRez
 const createGuest = async (first_name, last_name, email, phone) => {
@@ -48,7 +50,15 @@ const createGuest = async (first_name, last_name, email, phone) => {
 };
 
 // Function to create an inquiry in OwnerRez
-const createQuote = async (checkInDate, checkOutDate, guests, guestId, Children, Pets, PropertyId) => {
+const createQuote = async (
+  checkInDate,
+  checkOutDate,
+  guests,
+  guestId,
+  Children,
+  Pets,
+  PropertyId
+) => {
   try {
     const quotePayload = {
       GuestId: guestId,
@@ -57,7 +67,7 @@ const createQuote = async (checkInDate, checkOutDate, guests, guestId, Children,
       Adults: guests,
       Children: Children || 0,
       Pets: Pets || 0,
-      // PropertyId: PropertyId, // Ensure PropertyId is provided
+      PropertyId: PropertyId || 450490, // Ensure PropertyId is provided
     };
 
     const response = await axios.post(
@@ -78,17 +88,39 @@ const createQuote = async (checkInDate, checkOutDate, guests, guestId, Children,
   }
 };
 
+const getListProperties = async () => {
+  try {
+    const response = await axios.get(
+      "https://api.ownerrez.com/v2/properties",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: basicAuth, // Use Basic Auth
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error creating inquiry:", error.response?.data || error);
+    throw new Error("Failed to create inquiry");
+  }
+};
+
 // Function to process the webhook request
 const processWebhook = async (req, res) => {
   try {
-    const { first_name = "Hello", last_name = "world", custom_fields } = req.body;
+    const {
+      first_name = "Hello",
+      last_name = "world",
+      custom_fields,
+    } = req.body;
 
     const checkInDate = custom_fields["Check in Date"];
     const checkOutDate = custom_fields["Check Out Date"];
     const guests = custom_fields["Adults"] || 1; // Default to 1 adult if not provided
     const email = custom_fields["Email"] || "no-email@example.com";
     const phone = custom_fields["Phone"] || "+44 7723 476354";
-    const propertyId = custom_fields["Property ID"] || null;
+    const propertyId = custom_fields["Property ID"] || 450490;
     const children = custom_fields["Children"] || 0;
     const pets = custom_fields["Pets"] || 0;
 
@@ -99,7 +131,15 @@ const processWebhook = async (req, res) => {
     console.log("Guest Created Successfully with ID:", guestId);
 
     // Step 2: Create Inquiry
-    const quoteLink = await createQuote(checkInDate, checkOutDate, guests, guestId, children, pets, propertyId);
+    const quoteLink = await createQuote(
+      checkInDate,
+      checkOutDate,
+      guests,
+      guestId,
+      children,
+      pets,
+      propertyId
+    );
     console.log("Generated Quote Link:", quoteLink);
 
     // Step 3: Send response back to ManyChat
@@ -112,8 +152,21 @@ const processWebhook = async (req, res) => {
   }
 };
 
+const getProperties = async (req, res) => {
+  try {
+    const properties = await getListProperties();
+    return res.json({
+      properties,
+    });
+  } catch (error) {
+    console.error("Error processing webhook:", error);
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 // Webhook route
 app.post("/webhook/createInquiry", processWebhook);
+app.get("/list/properties", getProperties);
 
 // Start the server
 const PORT = process.env.PORT || 3000;
