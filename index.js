40f0bb8e4d3a5a3212c5b6ef560d9412942aaec9
Syed <syed.gohar@homelifemedia.com>
2025-03-25 12:49:05 +0500

    Changing Property Id

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const createQuote = async (
     };
 
     const response = await axios.post(
-      "https://api.ownerrez.com/v2/quotes",
+      "https://api.ownerrez.com/v1/quotes",
       quotePayload,
       {
         headers: {
